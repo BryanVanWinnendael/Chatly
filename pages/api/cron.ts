4bcb522da9877787e2d1dd203d73ac14297db82b
Bryan Van Winnendael <bryan.vanwinnendael@student.ucll.be>
2023-07-19 17:02:48 +0200

    added cron job

---
+++ pages/api/cron.ts
@@ -0,0 +1,29 @@
+import { NextApiRequest, NextApiResponse } from "next"
+import prisma from "../../lib/prisma"
+
+const getAllOpenRooms = async () => {
+  return await prisma.room.findMany({
+    where: {
+      status: "open",
+    },
+  })
+}
+
+const deleteAllOpenRooms = async () => {
+  return await prisma.room.deleteMany({
+    where: {
+      status: "open",
+    },
+  })
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  await deleteAllOpenRooms()
+  const rooms = await getAllOpenRooms()
+  console.log(rooms)
+
+  res.status(200).end("Cron job ran successfully")
+}
